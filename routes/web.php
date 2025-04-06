ba324abc31fdd92b8cc2527266d93701b3169d74
smit0164 <smitpatel.ast@gmail.com>
2025-04-06 18:26:54 +0530

    Route basic completed(normal route,route parameter,named route ,name routed with route parameter,fallback route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route parameter
+Route::get('/user/{id}/{slug}',function($slug,$po){
+      return "Hii,Your id is " . $slug.",".$po;
+});
+
+Route::get('/user/contact/{id}/{slug}',function($po,$pi){
+     return "Hii contact me on this id " . $po . " and slug is " . $pi;
+})->name('contact.me');
+
+Route::get('/user/example/{id}',function($pi){
+    return "this  is a example id " . $pi;
+})->name('example');
+
+Route::group(['prefix'=>'blog','as'=>'blog.'],function(){
+    Route::get('/create',function(){
+        return "This is a route for the create";
+    })->name('create');
+    Route::get('.update',function(){
+       return "This is a route for the update";
+    })->name('update');
+});
+
+Route::fallback(function(){
+    return "Sorry Page Not Found";
+});
